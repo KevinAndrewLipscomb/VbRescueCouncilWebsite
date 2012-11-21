556451a18d070e113b8eeb5ad302c831f383f886
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2012-11-21 20:15:32 +0000

    Creation.

---
+++ trunk/vb-rescue-council/website/component/db/Class_db_milestones.cs
@@ -0,0 +1,54 @@
+using Class_db;
+using Class_db_trail;
+using MySql.Data.MySqlClient;
+using System;
+
+namespace Class_db_milestones
+{
+    public class TClass_db_milestones: TClass_db
+    {
+        private TClass_db_trail db_trail = null;
+        //Constructor  Create()
+        public TClass_db_milestones() : base()
+        {
+            // TODO: Add any constructor code here
+            db_trail = new TClass_db_trail();
+        }
+
+        public void Check(uint code, out bool be_processed, out DateTime value)
+        {
+            be_processed = true;
+            value = DateTime.MaxValue;
+            Open();
+            var dr = new MySqlCommand
+              (
+              "select be_processed"
+              + " , value"
+              + " from fy_calendar"
+              + " where milestone_code = '" + code.ToString() + "'",
+              //+   " and fiscal_year_id = '" + biz_fiscal_years.IdOfCurrent() + "'"
+              connection
+              )
+              .ExecuteReader();
+            if (dr.Read())
+              {
+              be_processed = (dr["be_processed"].ToString() == "1");
+              value = DateTime.Parse(dr["value"].ToString());
+              }
+            dr.Close();
+            Close();
+        }
+
+        public void MarkProcessed(uint code)
+        {
+            string cmdText;
+            // + biz_fiscal_years.IdOfCurrent
+            cmdText = "update fy_calendar" + " set be_processed = TRUE" + " where fiscal_year_id = " + " and milestone_code = " + code.ToString();
+            this.Open();
+            new MySqlCommand(db_trail.Saved(cmdText), this.connection).ExecuteNonQuery();
+            this.Close();
+        }
+
+    } // end TClass_db_milestones
+
+}
