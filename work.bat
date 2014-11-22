29a55035c2123187e190761ed4dca1df0f53f0a3
kevinanlipscomb <kevinandrewlipscomb@vt.edu>
2014-11-22 22:50:44 +0000

    Revised to work on both Win32 and Win64.

--- work.bat
+++ work.bat
@@ -4,4 +4,4 @@ REM
 cd "C:\Inetpub\wwwroot\VbRescueCouncilWebsite"
 start /max explorer /e,/select,C:\Inetpub\wwwroot\VbRescueCouncilWebsite\.svn
 start /max VbRescueCouncilWebsite.sln
-start "" /max "C:\Program Files\MySQL\MySQL Workbench\MySQLWorkbench.exe"
+IF EXIST "C:\Program Files\MySQL\MySQL Workbench\MySQLWorkbench.exe" (start "" /max "C:\Program Files\MySQL\MySQL Workbench\MySQLWorkbench.exe") ELSE start "" /max "C:\Program Files (x86)\MySQL\MySQL Workbench\MySQLWorkbench.exe"
