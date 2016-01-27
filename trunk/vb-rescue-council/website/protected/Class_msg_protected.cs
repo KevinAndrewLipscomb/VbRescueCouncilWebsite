d5ddf839e96bc4fbeb38e96671dc036b36b72baa
kevinanlipscomb <kevinanlipscomb@fcafd3cc-6610-0410-92fd-86a883f3e63b>
2016-01-27 22:17:46 +0000

    Added role_member_map_log feature.

--- trunk/vb-rescue-council/website/protected/Class_msg_protected.cs
+++ trunk/vb-rescue-council/website/protected/Class_msg_protected.cs
@@ -5,10 +5,10 @@ namespace Class_msg_protected
   {
   public class TClass_msg_protected
     {
-    public class 0_detail
-      {
-      internal object summary = null;
-      }
+    //public class 0_detail
+    //  {
+    //  internal object summary = null;
+    //  }
     public class overview
       {
       internal string target = k.EMPTY;
